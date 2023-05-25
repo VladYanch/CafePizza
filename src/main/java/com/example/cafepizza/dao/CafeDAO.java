5d737ead19f5702ce54e54e9ed3c885f2940f26a
Vladyslav Yanchenko <vyanch72@gmail.com>
2023-05-25 11:07:24 +0200

    H2 подключение и исправление properties

--- src/main/java/com/example/cafepizza/dao/CafeDAO.java
+++ src/main/java/com/example/cafepizza/dao/CafeDAO.java
@@ -8,15 +8,9 @@ import java.util.List;
 
 @Component
 public class CafeDAO {
-    private static int CAFE_COUNT;
+
     private List<Cafe> cafeList;
 
-    {
-        cafeList = new ArrayList<>();
-        cafeList.add(new Cafe(++CAFE_COUNT,"Pomodoro"));
-        cafeList.add(new Cafe(++CAFE_COUNT,"Limonchelo"));
-        cafeList.add(new Cafe(++CAFE_COUNT,"Olio"));
-    }
 
     public List<Cafe> index() {
         return cafeList;
